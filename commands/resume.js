98bd4e492b196bd6c175bccb3d281c16ebb0e993
cinoxdz <125744743+cinoxdz@users.noreply.github.com>
2024-01-28 20:20:25 +0100

    Update resume.js

--- commands/resume.js
+++ commands/resume.js
@@ -25,8 +25,8 @@ module.exports = {
         .setColor('#7645fe')
         .setAuthor({
           name: 'Song Resumed',
-          iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157296313549983846/8929-purple-play-icon.png?ex=651817ae&is=6516c62e&hm=55fc041718da9277d1cdb13ef25ebf043d90588ee33c4bc838d9634ecfbc8e99&',
-          url: 'https://discord.gg/FUEHs7RCqz'
+          iconURL: 'https://media.discordapp.net/attachments/1183145662326526046/1183149629303427194/6095068ca3e711e9.png?ex=658748f5&is=6574d3f5&hm=84929c96a866ed158c57045fb8ba4194dd51a83dc13ce8814bb46c244bd6f2d5&',
+          url: 'https://discord.gg/ynk1'
         })
         .setDescription(success ? '**The music springs back to life!!**' : '❌ Error: Unable to resume song')
         
